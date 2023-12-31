0c0561e6c060d703727eff787cd9315d16eec904
bhumitjaviya54 <93394436+bhumitjaviya54@users.noreply.github.com>
2023-12-31 10:17:50 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// Operators In Js
+
+// Operators in JS are Symbols.
+
+// That are used to perform operations on operands.
+
+// Operands are the values and variables.
+
+// Types:--
+
+// Arithmetic Operators
+// Assignment Operators
+// Comparison Operators
+// Logical Operators
+// Striing Operators
+
+// 1. Arithmetic Operators
+
+// Arithmetic Operators are used to perform mathematical operations on operands.
+
+let sum = 5 + 3;
+let sub = 5 - 3;
+let mul = 5 * 3;
+let div = 5 / 3;
+let mod = 17 % 4; //moodulus operator
+let exp = 5 ** 3; //expoential operator
+
+console.log(sum);
+console.log(sub);
+console.log(mul);
+console.log(div);
+console.log(mod);
+console.log(exp);
